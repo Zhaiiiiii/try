8766f30feb01cbd09c28eafd24f0b8bcd823877c
Zhaira <zhairajeanebryne.cielo@cvsu.edu.ph>
2024-11-13 23:29:57 +0800

    login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Login</title>
+</head>
+<body>
+    <div class="container mt-5 pt-5">
+        <div class="row">
+            <div class="col-12 col-sm-8 col-md-6 md-auto mx-auto">
+                <div class="card" style="width: 30rem;">
+                    <div class="card-body mx-4">
+                        <h4 card-title>Admin</h4>
+                        <form action="">
+                            <div class="col-sm-11 mt-3">
+                                <input type="text" class="form-control mb-1" id="username" placeholder="Username" required>
+                                <input type="password" class="form-control" id="password" placeholder="Password" required>
+                            </div>
+                            <button class="btn btn-primary mt-4" style="width: 23rem;">Login</button>
+                            <a href="" class="nav-link mt-2 mb-2 text-center">Doesnt have an account?</a>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
